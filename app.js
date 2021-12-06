9388d08f851f207392e2415b70241005b8c326e4
vivekmehto <vivekmehto.webdev@gmail.com>
2021-12-06 20:26:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,6 +60,11 @@ const people = [
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
+const fifteenBorn = Array.inventors.filter(
+  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+);
+console.table(fifteenBorn);
+
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 
